7572bdef5668e6b0d2fad9928ce6850346bcac5b
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-08-15 17:07:08 +0530

    build docker image and push

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,13 +3,6 @@ import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "@eshan_tickets/common";
-import { NotFoundError } from "@eshan_tickets/common";
-
 const app = express();
 app.set("trust proxy", true);
 app.use(json());
@@ -19,16 +12,5 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
 
 export { app };
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -4,9 +4,9 @@ import { app } from "./app";
 import { DatabaseConnectionError } from "@eshan_tickets/common";
 
 const start = async () => {
-  if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY must be defined");
-  }
+  // if (!process.env.JWT_KEY) {
+  //   throw new Error("JWT_KEY must be defined");
+  // }
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {});
     console.log("Connected to MongoDb");
